89d28010aa6d6980b4e3363ce8a0a187031ebb06
usausa <machi.pon@gmail.com>
2018-10-21 11:59:21 +0900

    Update test

---
+++ ConverterWork/ConverterWork/Tests/NumericUIntToConvertTest.cs
@@ -0,0 +1,213 @@
+﻿namespace Smart.Tests
+{
+    using System;
+
+    using Smart.Converter2.Converters;
+
+    using Xunit;
+
+    public class NumericUIntToConvertTest
+    {
+        //--------------------------------------------------------------------------------
+        // SByteTo
+        //--------------------------------------------------------------------------------
+
+        [Fact]
+        public void UIntToByte()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal((byte)0, converter.Convert(UInt32.MinValue, typeof(byte)));
+            Assert.Equal((byte)255, converter.Convert(UInt32.MaxValue, typeof(byte)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+
+        [Fact]
+        public void UIntToNullableByte()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal((byte)0, converter.Convert(UInt32.MinValue, typeof(byte?)));
+            Assert.Equal((byte)255, converter.Convert(UInt32.MaxValue, typeof(byte?)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+
+        [Fact]
+        public void UIntToSByte()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal((sbyte)0, converter.Convert(UInt32.MinValue, typeof(sbyte)));
+            Assert.Equal((sbyte)-1, converter.Convert(UInt32.MaxValue, typeof(sbyte)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+
+        [Fact]
+        public void UIntToNullableSByte()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal((sbyte)0, converter.Convert(UInt32.MinValue, typeof(sbyte?)));
+            Assert.Equal((sbyte)-1, converter.Convert(UInt32.MaxValue, typeof(sbyte?)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+
+        [Fact]
+        public void UIntToShort()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal((short)0, converter.Convert(UInt32.MinValue, typeof(short)));
+            Assert.Equal((short)-1, converter.Convert(UInt32.MaxValue, typeof(short)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+
+        [Fact]
+        public void UIntToNullableShort()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal((short)0, converter.Convert(UInt32.MinValue, typeof(short?)));
+            Assert.Equal((short)-1, converter.Convert(UInt32.MaxValue, typeof(short?)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+
+        [Fact]
+        public void UIntToUShort()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal((ushort)0, converter.Convert(UInt32.MinValue, typeof(ushort)));
+            Assert.Equal((ushort)65535, converter.Convert(UInt32.MaxValue, typeof(ushort)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+
+        [Fact]
+        public void UIntToNullableUShort()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal((ushort)0, converter.Convert(UInt32.MinValue, typeof(ushort?)));
+            Assert.Equal((ushort)65535, converter.Convert(UInt32.MaxValue, typeof(ushort?)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+
+        [Fact]
+        public void UIntToInt()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal(0, converter.Convert(UInt32.MinValue, typeof(int)));
+            Assert.Equal(-1, converter.Convert(UInt32.MaxValue, typeof(int)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+
+        [Fact]
+        public void UIntToNullableInt()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal(0, converter.Convert(UInt32.MinValue, typeof(int?)));
+            Assert.Equal(-1, converter.Convert(UInt32.MaxValue, typeof(int?)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+
+        [Fact]
+        public void UIntToLong()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal(0L, converter.Convert(UInt32.MinValue, typeof(long)));
+            Assert.Equal(4294967295L, converter.Convert(UInt32.MaxValue, typeof(long)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+
+        [Fact]
+        public void UIntToNullableLong()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal(0L, converter.Convert(UInt32.MinValue, typeof(long?)));
+            Assert.Equal(4294967295L, converter.Convert(UInt32.MaxValue, typeof(long?)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+
+        [Fact]
+        public void UIntToULong()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal(0UL, converter.Convert(UInt32.MinValue, typeof(ulong)));
+            Assert.Equal(4294967295UL, converter.Convert(UInt32.MaxValue, typeof(ulong)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+
+        [Fact]
+        public void UIntToNullableULong()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal(0UL, converter.Convert(UInt32.MinValue, typeof(ulong?)));
+            Assert.Equal(4294967295UL, converter.Convert(UInt32.MaxValue, typeof(ulong?)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+
+        [Fact]
+        public void UIntToChar()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal((char)0, converter.Convert(UInt32.MinValue, typeof(char)));
+            Assert.Equal((char)65535, converter.Convert(UInt32.MaxValue, typeof(char)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+
+        [Fact]
+        public void UIntToNullableChar()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal((char)0, converter.Convert(UInt32.MinValue, typeof(char?)));
+            Assert.Equal((char)65535, converter.Convert(UInt32.MaxValue, typeof(char?)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+
+        [Fact]
+        public void UIntToDouble()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal(0d, converter.Convert(UInt32.MinValue, typeof(double)));
+            Assert.Equal(4294967295d, converter.Convert(UInt32.MaxValue, typeof(double)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+
+        [Fact]
+        public void UIntToNullableDouble()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal(0d, converter.Convert(UInt32.MinValue, typeof(double?)));
+            Assert.Equal(4294967295d, converter.Convert(UInt32.MaxValue, typeof(double?)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+
+        [Fact]
+        public void UIntToFloat()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal(0f, converter.Convert(UInt32.MinValue, typeof(float)));
+            Assert.Equal(4294967295f, converter.Convert(UInt32.MaxValue, typeof(float)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+
+        [Fact]
+        public void UIntToNullableFloat()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal(0f, converter.Convert(UInt32.MinValue, typeof(float?)));
+            Assert.Equal(4294967295f, converter.Convert(UInt32.MaxValue, typeof(float?)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+
+        [Fact]
+        public void UIntToDecimal()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal(0m, converter.Convert(UInt32.MinValue, typeof(decimal)));
+            Assert.Equal(4294967295m, converter.Convert(UInt32.MaxValue, typeof(decimal)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+
+        [Fact]
+        public void UIntToNullableDecimal()
+        {
+            var converter = new TestObjectConverter();
+            Assert.Equal(0m, converter.Convert(UInt32.MinValue, typeof(decimal?)));
+            Assert.Equal(4294967295m, converter.Convert(UInt32.MaxValue, typeof(decimal?)));
+            Assert.True(converter.UsedOnly<NumericConverterFactory>());
+        }
+    }
+}
