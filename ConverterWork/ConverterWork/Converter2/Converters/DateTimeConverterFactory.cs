89f0f2d4c86802720a516f60b4893b28fed5f871
usausa <machi.pon@gmail.com>
2018-10-22 17:54:07 +0900

    Update converters

--- ConverterWork/ConverterWork/Converter2/Converters/DateTimeConverterFactory.cs
+++ ConverterWork/ConverterWork/Converter2/Converters/DateTimeConverterFactory.cs
@@ -4,6 +4,8 @@
 
     public sealed class DateTimeConverterFactory : IConverterFactory
     {
+        // TODO Dic * 4
+
         private static readonly Type DateTimeType = typeof(DateTime);
 
         //private static readonly Type NullableDateTimeType = typeof(DateTime?);
@@ -14,6 +16,10 @@
 
         private static readonly Type StringType = typeof(string);
 
+        private static readonly Type LongType = typeof(long);
+
+        //private static readonly Type ULongType = typeof(ulong);
+
         public Func<object, object> GetConverter(IObjectConverter context, Type sourceType, Type targetType)
         {
             if (sourceType == DateTimeType)
@@ -23,6 +29,17 @@
                 {
                     return source => ((DateTime)source).ToString();
                 }
+
+                // To Long
+                targetType = targetType.IsNullableType() ? Nullable.GetUnderlyingType(targetType) : targetType;
+                if (targetType == LongType)
+                {
+                    return source => ((DateTime)source).Ticks;
+                }
+
+                // TODO long convert able?
+
+                return null;
             }
 
             if (sourceType == DateTimeOffsetType)
@@ -32,7 +49,30 @@
                 {
                     return source => ((DateTimeOffset)source).ToString();
                 }
+
+                // To Long
+                targetType = targetType.IsNullableType() ? Nullable.GetUnderlyingType(targetType) : targetType;
+                if (targetType == LongType)
+                {
+                    return source => ((DateTimeOffset)source).Ticks;
+                }
+
+                // TODO long convert able?
+
+                return null;
             }
+
+            //targetType = targetType.IsNullableType() ? Nullable.GetUnderlyingType(targetType) : targetType;
+            //if (targetType == DateTimeType)
+            //{
+            //    // From String
+            //    if (sourceType == StringType)
+            //    {
+            //        var defaultValue = targetType
+            //        return source => DateTime.TryParse((string)source, out var result) ? result : ;
+            //    }
+
+            //}
 
             // TODO DateTime <-> DateTimeOffset
             // TODO long
