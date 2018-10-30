a35f332cc4bcc2c107f48c6ec34cd43c01b03f50
usausa <machi.pon@gmail.com>
2018-10-30 22:44:25 +0900

    Update test base

--- ConverterWork/ConverterWork/Converter2/Converters/DefaultObjectFactories.cs
+++ ConverterWork/ConverterWork/Converter2/Converters/DefaultObjectFactories.cs
@@ -15,12 +15,12 @@
             {
                 new DBNullConverterFactory(),               // DBNull
                 new AssignableConverterFactory(),           // IsAssignableFrom
-                new EnumerableConverterFactory(),           // Enumerable
                 new BooleanConverterFactory(),              // Boolean
                 new NumericCastConverterFactory(),          // Numeric cast
                 new DecimalConverterFactory(),              // Decimal
                 new DateTimeConverterFactory(),             // DateTime/DateTimeOffset
                 new EnumConverterFactory(),                 // Enum to Enum, String to Enum, Assignable to Enum, Enum to Assignable
+                new EnumerableConverterFactory(),           // Enumerable
                 new ConversionOperatorConverterFactory(),   // Implicit/Explicit operator
                 new ToStringConverterFactory(),             // ToString finally
             };
---
+++ ConverterWork/ConverterWork/Tests/EnumerableConvertTest.cs
@@ -0,0 +1,141 @@
+﻿namespace Smart.Tests
+{
+    using Xunit;
+
+    public class EnumerableConvertTest
+    {
+        ////--------------------------------------------------------------------------------
+        //// To Array
+        ////--------------------------------------------------------------------------------
+
+        //// TODO Assignable/Enumerable
+
+        //[Fact]
+        //public void ArrayToSameElementArray()
+        //{
+        //    // TODO copy?
+        //}
+
+        //[Fact]
+        //public void ArrayToOtherElementArray()
+        //{
+        //    // TODO
+        //}
+
+        //[Fact]
+        //public void ListToSameElementArray()
+        //{
+        //    // TODO
+        //}
+
+        //[Fact]
+        //public void ListToOtherElementArray()
+        //{
+        //    // TODO
+        //}
+
+        //[Fact]
+        //public void ArrayListToSameElementArray()
+        //{
+        //    // TODO
+        //}
+
+        //[Fact]
+        //public void ArrayListToOtherElementArray()
+        //{
+        //    // TODO
+        //}
+
+        ////--------------------------------------------------------------------------------
+        //// To List
+        ////--------------------------------------------------------------------------------
+
+        //[Fact]
+        //public void ListToSameElementList()
+        //{
+        //    // TODO
+        //}
+
+        //[Fact]
+        //public void ListToOtherElementList()
+        //{
+        //    // TODO
+        //}
+
+        //[Fact]
+        //public void ArrayToSameElementList()
+        //{
+        //    // TODO
+        //}
+
+        //[Fact]
+        //public void ArrayToOtherElementList()
+        //{
+        //    // TODO
+        //}
+
+        //[Fact]
+        //public void ArrayListToSameElementList()
+        //{
+        //    // TODO
+        //}
+
+        //[Fact]
+        //public void ArrayListToOtherElementList()
+        //{
+        //    // TODO
+        //}
+
+        ////--------------------------------------------------------------------------------
+        //// To Set
+        ////--------------------------------------------------------------------------------
+
+        //[Fact]
+        //public void SetToSameElementSet()
+        //{
+        //    // TODO
+        //}
+
+        //[Fact]
+        //public void SetToOtherElementSet()
+        //{
+        //    // TODO
+        //}
+
+        //[Fact]
+        //public void ArrayToSameElementSet()
+        //{
+        //    // TODO
+        //}
+
+        //[Fact]
+        //public void ArrayToOtherElementSet()
+        //{
+        //    // TODO
+        //}
+
+        //[Fact]
+        //public void ListToSameElementSet()
+        //{
+        //    // TODO
+        //}
+
+        //[Fact]
+        //public void ListToOtherElementSet()
+        //{
+        //    // TODO
+        //}
+
+        //[Fact]
+        //public void ArrayListToSameElementSet()
+        //{
+        //    // TODO
+        //}
+
+        //[Fact]
+        //public void ArrayListToOtherElementSet()
+        //{
+        //    // TODO
+        //}
+    }
+}
