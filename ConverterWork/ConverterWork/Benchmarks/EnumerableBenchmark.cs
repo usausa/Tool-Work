4fc8c5ec72589ca4430708c376d2f3a13ff4fced
usausa <machi.pon@gmail.com>
2018-11-03 16:48:27 +0900

    Add enumrable benchmark

---
+++ ConverterWork/ConverterWork/Benchmarks/EnumerableBenchmark.cs
@@ -0,0 +1,63 @@
+﻿namespace Smart.Benchmarks
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using BenchmarkDotNet.Attributes;
+
+    [Config(typeof(BenchmarkConfig))]
+    public class EnumerableBenchmark
+    {
+        private readonly List<int> list = new List<int>();
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                list.Add(i);
+            }
+        }
+
+        [Benchmark]
+        public int For()
+        {
+            var total = 0;
+            for (var i = 0; i < list.Count; i++)
+            {
+                total += list[i];
+            }
+
+            return total;
+        }
+
+        [Benchmark]
+        public int ForEach()
+        {
+            var total = 0;
+            foreach (var i in list)
+            {
+                total += i;
+            }
+
+            return total;
+        }
+
+        [Benchmark]
+        public int Enumerator()
+        {
+            var total = 0;
+            using (var ie = list.GetEnumerator())
+            {
+                while (ie.MoveNext())
+                {
+                    total += ie.Current;
+                }
+            }
+
+            return total;
+        }
+    }
+}
