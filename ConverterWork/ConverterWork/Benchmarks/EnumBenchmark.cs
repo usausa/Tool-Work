6e131aa18fd0695b6dc6d5f79f71b9473b782a6c
usausa <machi.pon@gmail.com>
2018-11-09 17:46:24 +0900

    Add enum benchmark

---
+++ ConverterWork/ConverterWork/Benchmarks/EnumBenchmark.cs
@@ -0,0 +1,47 @@
+﻿namespace Smart.Benchmarks
+{
+    using System;
+
+    using BenchmarkDotNet.Attributes;
+
+    [Config(typeof(BenchmarkConfig))]
+    public class EnumBenchmark
+    {
+        private enum MyEnum
+        {
+            One,
+            Two,
+            Three,
+            Four,
+            Five
+        }
+
+        private static readonly MyEnum EnumValue = MyEnum.Three;
+
+        private static readonly Type EnumType = typeof(MyEnum);
+
+        [Benchmark]
+        public object EnumToString()
+        {
+            return EnumValue.ToString();
+        }
+
+        [Benchmark]
+        public object EnumToStringByHelper()
+        {
+            return EnumHelper<MyEnum>.GetName(EnumValue);
+        }
+
+        [Benchmark]
+        public object EnumFromString()
+        {
+            return (MyEnum)Enum.Parse(EnumType, "Two");
+        }
+
+        [Benchmark]
+        public object EnumFromStringByHelper()
+        {
+            return EnumHelper<MyEnum>.ParseValue("Two");
+        }
+    }
+}
