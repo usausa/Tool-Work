513760194261aba0764dd8b91b269acf0a3a1155
usausa <machi.pon@gmail.com>
2018-11-10 19:42:14 +0900

    Update benchmark

--- ConverterWork/ConverterWork/Benchmarks/EnumBenchmark.cs
+++ ConverterWork/ConverterWork/Benchmarks/EnumBenchmark.cs
@@ -20,6 +20,23 @@
 
         private static readonly Type EnumType = typeof(MyEnum);
 
+        private Func<object, object> toStringByMethod;
+
+        private Func<object, object> fromStringByMethod;
+
+        private Func<object, object> toStringByClass;
+
+        private Func<object, object> fromStringByClass;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            toStringByMethod = ToStringBuilderMethod();
+            fromStringByMethod = FromStringBuilderMethod();
+            toStringByClass = new ToStringClass().Convert;
+            fromStringByClass = new FromStringClass().Convert;
+        }
+
         [Benchmark]
         public object EnumToString()
         {
@@ -33,6 +50,18 @@
         }
 
         [Benchmark]
+        public object EnumToStringByMethod()
+        {
+            return toStringByMethod(EnumValue);
+        }
+
+        [Benchmark]
+        public object EnumToStringByClass()
+        {
+            return toStringByClass(EnumValue);
+        }
+
+        [Benchmark]
         public object EnumFromString()
         {
             return (MyEnum)Enum.Parse(EnumType, "Two");
@@ -42,6 +71,44 @@
         public object EnumFromStringByHelper()
         {
             return EnumHelper<MyEnum>.ParseValue("Two");
+        }
+
+        [Benchmark]
+        public object EnumFromStringByMethod()
+        {
+            return (MyEnum)fromStringByMethod("Two");
+        }
+
+        [Benchmark]
+        public object EnumFromStringByClass()
+        {
+            return (MyEnum)fromStringByClass("Two");
+        }
+
+        private Func<object, object> ToStringBuilderMethod()
+        {
+            return source => EnumHelper<MyEnum>.GetName((MyEnum)source);
+        }
+
+        private Func<object, object> FromStringBuilderMethod()
+        {
+            return source => EnumHelper<MyEnum>.TryParseValue((string)source, out var value) ? value : default;
+        }
+
+        private sealed class ToStringClass
+        {
+            public object Convert(object source)
+            {
+                return EnumHelper<MyEnum>.GetName((MyEnum)source);
+            }
+        }
+
+        private sealed class FromStringClass
+        {
+            public object Convert(object source)
+            {
+                return EnumHelper<MyEnum>.TryParseValue((string)source, out var value) ? value : default;
+            }
         }
     }
 }
